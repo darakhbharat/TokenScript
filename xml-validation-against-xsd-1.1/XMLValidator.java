72b9019ce504f015c961e06e66e0e964aa6055f4
darakhbharat <darakhbharat@gmail.com>
2020-11-16 15:21:11 +0530

    1. Resolved issue with the XML -deref and done some code clean up. 2. Work in progrees on XSD validation Using schemaLocation

--- xml-validation-against-xsd-1.1/XMLValidator.java
+++ xml-validation-against-xsd-1.1/XMLValidator.java
@@ -1,33 +1,47 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Iterator;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
+import org.apache.xerces.util.XMLCatalogResolver;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 
 public class XMLValidator {
 	private static final String W3C_XML_SCHEMA_11_NS_URI = "http://www.w3.org/XML/XMLSchema/v1.1";
 	private static final String XML_VALIDATION = "-val";
 	private static final String ENTITY_DEREFERENCE = "-deref";
+	private static boolean actionValidate = true;
+	private static boolean actionEntityDeRef = false;
 	
 	private static void validateFile(File xmlFile, File xsdFile) throws SAXException, IOException {
 	    // 1. Lookup a factory for the W3C XML Schema language
@@ -44,14 +58,11 @@ public class XMLValidator {
 	    Source source = new StreamSource(xmlFile);
 
 	    // 5. Check the document
-	    try
-	    {
+	    try {
 	        validator.validate(source);
-	        System.out.println(xmlFile.getName() + " is valid.");
-	    }
-	    catch (SAXException ex)
-	    {
-	        System.out.println(xmlFile.getName() + " is not valid because ");
+	        System.out.println(xmlFile.getName() + " [VALID]");
+	    } catch (SAXException ex) {
+	        System.out.println(xmlFile.getName() + " [NOT VALID]");
 	        System.out.println(ex.getMessage());
 	    }
 	}
@@ -61,23 +72,75 @@ public class XMLValidator {
 			if (file.isDirectory())
 				findXMLFileAndValidate(file, xsdFile);
 			String filename = file.getName();
-			if (file.isFile() && filename.endsWith(".xml")) {
-				XMLValidator.validateFile(file, xsdFile);
+			if (file.isFile() && filename.endsWith(".xml") && xsdFile != null && xsdFile.exists()) {
+				validateFile(file, xsdFile);
+			} else {
+				validateUsingschemaLocation(file);
 			}
 		}
 	}
 	
-	private static void entityDeReferencing(File xmlFile){
+	private static void validateUsingschemaLocation(File file) {
+	    try {
+	      
+	    	
+	    	String [] catalogs =  
+	    		  {file.getAbsolutePath()};
+	    	// Create catalog resolver and set a catalog list.
+	    	XMLCatalogResolver resolver = new XMLCatalogResolver();
+	    	resolver.setPreferPublic(true);
+	    	resolver.setCatalogList(catalogs);
+
+	    	
+	    	
+	      String parserClass = "org.apache.xerces.parsers.SAXParser";
+	      String schemaFeature = "http://apache.org/xml/features/validation/schema";
+
+	      // getting Xerces SAX parser as an XMLReader
+	      XMLReader parser
+	         = XMLReaderFactory.createXMLReader(parserClass);
+
+	      // setting the parser for XML schema validation
+	      parser.setFeature(schemaFeature,true);
+	      
+	      // Set the resolver on the parser.
+	      parser.setProperty("http://apache.org/xml/properties/internal/entity-resolver", resolver);
+	      //parser.setErrorHandler(new MyErrorHandler());
+	      InputStream in = new FileInputStream(file);
+	      InputSource inputSource = new InputSource(new InputStreamReader(in));
+	      System.out.println();
+	      System.out.println("Parsing and validating: "+file.getName());
+	      parser.parse(inputSource);
+	      System.out.println();
+	      
+	    } catch (Exception e) {
+	      // catching all exceptions
+	      System.out.println();
+	      System.out.println(e.toString());
+	    }
+	  }
+	private static void entityDeReferencing(File xmlFile) throws XPathExpressionException{
 		try{
 			
 			String newFileName = xmlFile.getAbsolutePath().substring(0, xmlFile.getAbsolutePath().indexOf(".xml"))+"-entity-deref.xml";
 			
 			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document doc = builder.parse(xmlFile);
+			
+			XPath xPath = XPathFactory.newInstance().newXPath();
+            XPathExpression exp = xPath.compile("//@*[local-name() = 'base']");
+            NodeList nodeList = (NodeList)exp.evaluate(doc, XPathConstants.NODESET);
+            
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Attr attr = (Attr) nodeList.item(i);
+                Element el = ((Attr) nodeList.item(i)).getOwnerElement();
+                el.removeAttributeNode(attr);
+               
+            }
 			//doc.getDocumentElement().normalize();
 			DOMSource ds = new DOMSource(doc);
 			Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		 
+			//StreamResult result = new StreamResult(System.out);
 		    StreamResult result = new StreamResult(new File(newFileName));
 		    transformer.transform(ds, result);
 		} catch (TransformerException e) {
@@ -91,7 +154,7 @@ public class XMLValidator {
 		}
 	}
 	
-	private static void findXMLFileAndEntityDeRef(File dir) throws IOException, SAXException {
+	private static void findXMLFileAndEntityDeRef(File dir) throws IOException, SAXException, XPathExpressionException {
 		File[] listOfFiles = dir.listFiles();
 		for (File file : listOfFiles) {
 			if (file.isDirectory())
@@ -106,16 +169,12 @@ public class XMLValidator {
 	public static void main(String[] args) { 
 		if(args.length < 2){
 	        System.out.println("Please provide a valid command line arguments to get better results.");
-	        //System.exit(0);
+	        System.exit(0);
 	    }
-		boolean actionValidate = false;
-		boolean actionEntityDeRef = false;
 		
 		File xsdFile = null;
 		File xmlFile = null;
 		
-		//boolean actionValidate = true;
-		//boolean actionEntityDeRef = true;
 		//File xmlFile = new File("H:/alphawallet/tokenscripts/COFI.xml");
 		//H:/alphawallet/TokenScript-Examples/examples/Karma/karma.xml
 		//File xmlFile = new File("H:/alphawallet/TokenScript-Examples/examples/Karma/karma.xml");
@@ -136,10 +195,12 @@ public class XMLValidator {
 		
 		if(actionValidate){
 			try {
-				if(xmlFile.isDirectory()){
-					XMLValidator.findXMLFileAndValidate(xmlFile, xsdFile);
-				} else {
-					XMLValidator.validateFile(xmlFile, xsdFile);
+				if(xmlFile.isDirectory() && xsdFile != null && xsdFile.exists()){
+					findXMLFileAndValidate(xmlFile, xsdFile);
+				} else if(xmlFile.isFile() && xsdFile != null && xsdFile.exists()){
+					validateFile(xmlFile, xsdFile);
+				} else{
+					validateUsingschemaLocation(xmlFile);
 				}
 			} catch (SAXException | IOException e) {
 				e.printStackTrace();
@@ -156,6 +217,9 @@ public class XMLValidator {
 					entityDeReferencing(xmlFile);
 				}
 			} catch (SAXException | IOException e) {
+				e.printStackTrace();
+			} catch (XPathExpressionException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} finally{
 				System.out.println("Entity dereference action completed");
